7f7854a4f7ef5705e0acaf341f180d75ef915c90
Emery E. Lee <154424574+eeleedev@users.noreply.github.com>
2024-12-13 00:12:37 +0900

    Title: 특정 숫자 도달하기, Time: 49ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241213/특정 숫자 도달하기/reaching-specific-number.py
@@ -0,0 +1,15 @@
+arr = list(map(int, input().split()))
+n = len(arr)
+
+def find_the_end(n):
+    for i in range(n):
+        if arr[i] >= 250 :
+            return i
+
+end = find_the_end(n)
+
+arr_new = arr[0:end]
+all_sum = 0
+for i in arr_new:
+    all_sum += i
+print(all_sum, all_sum/len(arr_new))
