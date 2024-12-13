68f6d056fac2ddf2f9e006353184041dd3e5b28f
Emery E. Lee <154424574+eeleedev@users.noreply.github.com>
2024-12-13 12:49:44 +0900

    Title: 배열 채우기, Time: 77ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241213/배열 채우기/filling-array.py
@@ -0,0 +1,17 @@
+arr = list(map(int,input().split()))
+
+def find_index():
+    for elem in arr:
+        if elem == 0:
+            return arr.index(elem)
+
+def final_result():
+    n = find_index()
+    if n != None:
+        for i in range(n-1, -1, -1):
+            print(arr[i], end=" ")
+    else:
+        for i in range(10, -1, -1):
+            print(arr[i], end=" ")
+
+final_result()
