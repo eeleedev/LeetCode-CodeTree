767eecc683faf0475c0d2771c9f3df8b0a4ee6ba
Emery E. Lee <154424574+eeleedev@users.noreply.github.com>
2024-12-13 13:03:52 +0900

    Title: 배열 채우기, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 241213/배열 채우기/filling-array.py
+++ 241213/배열 채우기/filling-array.py
@@ -1,18 +1,17 @@
-arr = list(map(int,input().split()))
+arr = list(map(int, input().split()))
 
 def find_index():
-    for elem in arr:
-        if elem == 0:
-            return arr.index(elem)
-        return -1
+    if 0 in arr:  # 리스트에 0이 있는지 확인
+        return arr.index(0)  # 0의 첫 번째 위치 반환
+    return -1  # 0이 없으면 -1 반환
 
 def final_result():
     n = find_index()
-    if n != -1:
-        for i in range(len(arr)-1, -1, -1):
+    if n != -1:  # 0이 있는 경우
+        for i in range(n - 1, -1, -1):  # 0의 앞 요소부터 역순으로 출력
             print(arr[i], end=" ")
-    else:
-        for i in range(len(arr)-2, -1, -1):
+    else:  # 0이 없는 경우 전체를 역순으로 출력
+        for i in range(len(arr) - 1, -1, -1):  # 전체 리스트에 대해 역순 출력
             print(arr[i], end=" ")
 
 final_result()
