4d74c23cdb26950cc428eab6ebd2d78f6e62e951
Emery E. Lee <154424574+eeleedev@users.noreply.github.com>
2024-12-31 00:52:46 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 39ms, Memory: 16MB, Status: Passed - CodeTree

--- 241231/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
+++ 241231/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -1,20 +1,16 @@
 n = int(input())
-arr = list(map(int,input().split()))
+arr = list(map(int, input().split()))
 
-def gcd(a,b):
+def gcd(a, b):
     if b == 0:
         return a
-    if a > b:
-        return gcd(b, a%b)
-    if b > a:
-        return gcd(a, b%a)
+    return gcd(b, a % b)
 
 def lcm(arr):
     if len(arr) == 1:
         return arr[0]
-    new_value = arr[0]*arr[1]//gcd(arr[0],arr[1]) # 최소공배수는 정수로 계산해야 함(정수 나눗셈 사용)
-    arr =[new_value] + arr[2:]
+    new_value = (arr[0] * arr[1]) // gcd(arr[0], arr[1])  # 정수 나눗셈으로 수정
+    arr = [new_value] + arr[2:]
     return lcm(arr)
 
-print(lcm(arr))
-    
+print(lcm(arr))  # int() 불필요 (이미 정수)
