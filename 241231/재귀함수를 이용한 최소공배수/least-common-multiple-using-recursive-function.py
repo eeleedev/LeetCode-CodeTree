28cd174ef9665a59c32cd0b8d72f0b0380af7c14
Emery E. Lee <154424574+eeleedev@users.noreply.github.com>
2024-12-31 00:52:45 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 73ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241231/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
+++ 241231/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -12,9 +12,9 @@ def gcd(a,b):
 def lcm(arr):
     if len(arr) == 1:
         return arr[0]
-    new_value = arr[0]*arr[1]/gcd(arr[0],arr[1])
+    new_value = arr[0]*arr[1]//gcd(arr[0],arr[1]) # 최소공배수는 정수로 계산해야 함(정수 나눗셈 사용)
     arr =[new_value] + arr[2:]
     return lcm(arr)
 
-print(int(lcm(arr)))
+print(lcm(arr))
     
