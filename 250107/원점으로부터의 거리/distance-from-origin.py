df4213ae6aaac6f621089e3240b59c258ce9293e
Emery E. Lee <154424574+eeleedev@users.noreply.github.com>
2025-01-07 22:12:26 +0900

    Title: 원점으로부터의 거리, Time: 70ms, Memory: 17MB, Status: Passed - CodeTree

--- 250107/원점으로부터의 거리/distance-from-origin.py
+++ 250107/원점으로부터의 거리/distance-from-origin.py
@@ -8,7 +8,7 @@ n = int(input())
 arr = [(i+1, *map(int, input().split())) for i in range(n)]
 numbers = [Number(order, x, y) for order, x, y in arr]
 
-numbers.sort(key=lambda x: (abs(x.x+x.y), x.order))
+numbers.sort(key=lambda x: (abs(x.x)+abs(x.y), x.order))
 
 
 for number in numbers:
