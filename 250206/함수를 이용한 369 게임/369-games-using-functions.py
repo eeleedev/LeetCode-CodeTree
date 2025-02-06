1f605f0dd7d53446df44665c77d791162ca437fd
Emery E. Lee <154424574+eeleedev@users.noreply.github.com>
2025-02-06 15:20:03 +0900

    Title: 함수를 이용한 369 게임, Time: 115ms, Memory: 17MB, Status: Passed - CodeTree

--- 250206/함수를 이용한 369 게임/369-games-using-functions.py
+++ 250206/함수를 이용한 369 게임/369-games-using-functions.py
@@ -1,21 +1,19 @@
 a, b = map(int, input().split())
 
 def tsn(num):
-    temp1 = num % 10
-    temp2 = num // 10
-    if temp1 == 3 or temp1 == 6 or temp1 == 9:
-        return True
-    elif temp2 == 3 or temp2 == 6 or temp2 == 9:
-        return True
+    temp = list(str(num))
+    for a in temp:
+        if a == '3' or a=='6' or a=='9':
+            return True
     else:
         return False
 
 def count_func(a,b):
     counter = 0
     for i in range(a,b+1):
-        if tsn(i):
+        if i%3==0:
                 counter += 1
-        elif i%3==0:
+        elif tsn(i):
             counter += 1
     return counter
 
