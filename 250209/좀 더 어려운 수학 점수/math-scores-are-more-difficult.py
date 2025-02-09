08c661d52944cddd7aa0359ca1b22793307ba3f0
Emery E. Lee <154424574+eeleedev@users.noreply.github.com>
2025-02-09 18:37:33 +0900

    Title: 좀 더 어려운 수학 점수, Time: 53ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250209/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -0,0 +1,12 @@
+A_m, A_e = map(int, input().split())
+B_m, B_e = map(int, input().split())
+
+if A_m > B_m:
+    print("A")
+elif A_m < B_m:
+    print("B")
+else:
+    if A_e > B_e:
+        print("A")
+    else:
+        print("B")
