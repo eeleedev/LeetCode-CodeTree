1ec1e8fff4a696b34bf1941ccc68ce5947125035
Emery E. Lee <154424574+eeleedev@users.noreply.github.com>
2025-02-21 17:35:30 +0900

    Title: 이상한 진수 2, Time: 40ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250221/이상한 진수 2/awkward-digits-2.py
+++ 250221/이상한 진수 2/awkward-digits-2.py
@@ -4,7 +4,7 @@ a = input()
 max_value = int(a, 2)  # 초기값을 현재 숫자의 십진수 값으로 설정
 
 # 3. 두 번째 비트부터 하나씩 바꿔보면서 최댓값 찾기
-for i in range(1, len(a)):  # 첫 번째 비트(0번 인덱스)는 변경하지 않음
+for i in range(0, len(a)):
     # 현재 비트를 반전 (0 → 1 또는 1 → 0)한 새로운 문자열 생성
     # 문자열은 불변(immutable)이기 때문에 temp[i] 값을 직접 변경할 수 없음
     flipped = a[:i] + ('1' if a[i] == '0' else '0') + a[i+1:]
