89fe048dbb141f2bd0557a3659058ea6b49bf5c5
Emery E. Lee <154424574+eeleedev@users.noreply.github.com>
2025-02-26 00:07:37 +0900

    Title: 십의 자리 숫자, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250226/십의 자리 숫자/number-of-tens-digit.py
+++ 250226/십의 자리 숫자/number-of-tens-digit.py
@@ -1,11 +1,15 @@
 input_list = list(map(int, input().split()))
-count_list = [0]*(len(input_list)-1)
+count_list = [0]*9 # 10의 자리 숫자 9개
 
 for i in range(len(input_list)):
-    temp = input_list[i]//10
-    if temp == 0:
-        continue
-    count_list[temp-1] += 1
+    if input_list[i] == 0:
+        break
+    else:
+        temp = input_list[i]//10
+        if temp == 0:
+            continue
+        count_list[temp-1] += 1
+
 
 for i in range(len(count_list)):
     print(i+1,'-',count_list[i])
