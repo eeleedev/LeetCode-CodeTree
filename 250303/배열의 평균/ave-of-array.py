67f5ad2097f2339d8ee61c4ab65585509b8436fc
Emery E. Lee <154424574+eeleedev@users.noreply.github.com>
2025-03-03 00:02:21 +0900

    Title: 배열의 평균, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250303/배열의 평균/ave-of-array.py
@@ -0,0 +1,25 @@
+n = 2
+k = 4
+arr_2d = []
+for _ in range(n):
+    arr_1d = list(map(int, input().split()))
+    arr_2d.append(arr_1d)
+
+for i in range(n):
+    average = sum(arr_2d[i])/k
+    print(round(average,2),end=' ')
+
+print()
+
+for i in range(k):
+    average = (arr_2d[0][i]+arr_2d[1][i])/n
+    print(round(average,2),end=' ')
+
+total_sum = 0
+
+for i in range(n):
+    for j in range(k):
+        total_sum += arr_2d[i][j]
+
+print()
+print(total_sum/8)
