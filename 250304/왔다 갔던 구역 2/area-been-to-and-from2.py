6c77b215cc47f9b696bfb16eff5fcc3e7ded3ebc
Emery E. Lee <154424574+eeleedev@users.noreply.github.com>
2025-03-04 21:31:58 +0900

    Title: 왔다 갔던 구역 2, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250304/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,28 @@
+n = int(input())
+x = []
+dir = []
+for _ in range(n):
+    xi, di = input().split()
+    x.append(int(xi))
+    dir.append(di)
+
+# Please write your code here.
+lines = [0] * 202 # -100 ~ 100
+start_point = 100 # 0 좌표를 100으로 매핑
+
+for i in range(n):
+    if dir[i] == 'R':
+        for _ in range(x[i]):
+            lines[start_point] += 1
+            start_point += 1  # 오른쪽으로 이동
+    elif dir[i] == 'L':
+        for _ in range(x[i]):
+            lines[start_point] += 1
+            start_point -= 1  # 왼쪽으로 이동
+
+count = 0
+for c in lines:
+    if c >= 2:
+        count += 1
+
+print(count)
